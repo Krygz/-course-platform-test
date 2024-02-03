450fe9458d39e28eea2c7290712cbb4ee611f89b
Pedro <ph4290245@gmail.com>
2024-02-03 10:38:25 -0300

    enrollment seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -17,8 +17,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 INSERT INTO tb_course(name , img_Uri , img_Gray_Uri) VALUES ('Bootcamp Html' , 'https://static.vecteezy.com/system/resources/thumbnails/010/869/731/small/online-education-concept-illustration-digital-classroom-online-teaching-metaphors-free-png.png' , 'https://icons.veryicon.com/png/o/miscellaneous/light-e-treasure-3/training-course.png');
 
 
-INSERT INTO tb_offer(edition , start_Moment , end_Moment , course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z' ,TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z','1' );
-INSERT INTO tb_offer(edition , start_Moment , end_Moment , course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z' ,TIMESTAMP WITH TIME ZONE '2025-11-20T03:00:00Z' ,'1');
+INSERT INTO tb_offer(edition , start_Moment , end_Moment , course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z' ,TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z',1 );
+INSERT INTO tb_offer(edition , start_Moment , end_Moment , course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z' ,TIMESTAMP WITH TIME ZONE '2025-11-20T03:00:00Z' ,1);
 
 INSERT INTO tb_resource(title , description , position , img_Uri , type , offer_id) VALUES ('trilha Html' , 'trilha principal do curso', 1, 'https://static.vecteezy.com/system/resources/thumbnails/010/869/731/small/online-education-concept-illustration-digital-classroom-online-teaching-metaphors-free-png.png', 'LESSON_TASK', 1);
 INSERT INTO tb_resource(title , description , position , img_Uri , type , offer_id) VALUES ('Forum' , 'tire suas duvidas', 2, 'https://static.vecteezy.com/system/resources/thumbnails/010/869/731/small/online-education-concept-illustration-digital-classroom-online-teaching-metaphors-free-png.png', 'FORUM', 1);
@@ -28,3 +28,5 @@ INSERT INTO tb_section(title , description , position , img_Uri ,resource_id , p
 INSERT INTO tb_section(title , description , position , img_Uri ,resource_id , prerequisite_id) VALUES ('Capitulo 2' , 'Continuação', 2, 'https://static.vecteezy.com/system/resources/thumbnails/010/869/731/small/online-education-concept-illustration-digital-classroom-online-teaching-metaphors-free-png.png',1, 1);
 INSERT INTO tb_section(title , description , position , img_Uri ,resource_id , prerequisite_id) VALUES ('Capitulo 3' , 'Final', 3, 'https://static.vecteezy.com/system/resources/thumbnails/010/869/731/small/online-education-concept-illustration-digital-classroom-online-teaching-metaphors-free-png.png',1, 2);
 
+INSERT INTO tb_enrollment(user_id , offer_id , enroll_Moment , refund_Moment , available , only_Update) VALUES (1,1, TIMESTAMP WITH TIME ZONE '2020-11-21T03:00:00Z',null ,true , false);
+INSERT INTO tb_enrollment(user_id , offer_id , enroll_Moment , refund_Moment , available , only_Update) VALUES (2,1, TIMESTAMP WITH TIME ZONE '2020-11-21T03:00:00Z',null ,true , false);
